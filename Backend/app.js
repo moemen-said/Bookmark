a469d9bd876e7d2d97f2a656ab0896b868d4833f
Moemen Said <MoemenSaid@users.noreply.github.com>
2022-02-10 15:28:16 +0200

    first deploy

--- Backend/app.js
+++ Backend/app.js
@@ -1,31 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const compression = require("compression");
 
-const authRoutes = require('./routes/auth');
-const storeRoutes = require('./routes/store');
-const checkAuth = require('./middlewares/checkAuth');
+const authRoutes = require("./routes/auth");
+const storeRoutes = require("./routes/store");
+const checkAuth = require("./middlewares/checkAuth");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://moemen:moemenPass@cluster0.cfkkx.mongodb.net/Bookmark')
+mongoose
+    .connect("mongodb+srv://moemen:moemenPass@cluster0.cfkkx.mongodb.net/Bookmark")
     .then(() => {
-        console.log("Connected Successfully to Database")
+        console.log("Connected Successfully to Database");
     })
     .catch(() => {
-        console.log("Could Not Connect to Database")
-    })
+        console.log("Could Not Connect to Database");
+    });
 
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(compression());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
-})
+});
 
-app.use('/api/auth', authRoutes)
-app.use('/api/store', storeRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/store", storeRoutes);
 
 app.listen(3000);
